001a53c5080f9ce7933fd0b98f0e424853958e8a
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-09-10 00:43:12 +0600

    Hero section & Background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        strongSyan: 'hsl(171, 66%, 44%)',
+        strongCyan: 'hsl(171, 66%, 44%)',
         lightBlue: 'hsl(233, 100%, 69%)',
         darkGrayishBlue: 'hsl(210, 10%, 33%)',
         grayishBlue: 'hsl(201, 11%, 66%)',
